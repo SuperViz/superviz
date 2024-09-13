a504a87b6d07b5b72b7112b98666601e6622b2c2
Carlos <carlos.santos74@outlook.com>
2024-09-13 09:05:35 -0300

    chore: add eslint and ts base configs

--- packages/eslint-config/library.js
+++ packages/eslint-config/library.js
@@ -4,8 +4,8 @@ const project = resolve(process.cwd(), "tsconfig.json");
 
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ["eslint:recommended", "prettier", "turbo"],
-  plugins: ["only-warn"],
+  extends: ["eslint:recommended", "prettier", "airbnb-base", "plugin:import/recommended", "plugin:import/typescript"],
+  plugins: ["only-warn", "@typescript-eslint", "eslint-plugin-import"],
   globals: {
     React: true,
     JSX: true,
@@ -19,6 +19,53 @@ module.exports = {
         project,
       },
     },
+  },
+  rules: {
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'no-unused-vars': 'off',
+    'no-shadow': 'off',
+    'no-prototype-builtins': 'off',
+    'prefer-spread': 'off',
+    'no-console': 'off',
+    'class-methods-use-this': 'off',
+    'import/prefer-default-export': 'off',
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'always',
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'object'],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: false,
+        },
+        pathGroups: [
+          {
+            pattern: '@/**',
+            group: 'external',
+            position: 'after',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['builtin'],
+      },
+    ],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'always',
+        ts: 'never',
+      },
+    ],
+    'lines-between-class-members': ['error', 'always', { exceptAfterSingleLine: true }],
+    'prefer-object-spread': 'off',
+    'no-underscore-dangle': 'off',
+    'object-curly-newline': 'off',
+    'operator-linebreak': 'off',
+    'no-use-before-define': 'off',
+    'arrow-body-style': 'off',
+    'no-plusplus': 'off',
+    'consistent-return': 'off',
+    'dot-notation': 'off',
   },
   ignorePatterns: [
     // Ignore dotfiles
