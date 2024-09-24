92df60aaeff51689225600c70425c5b33ac61cf6
Ian Silva <raspincel@gmail.com>
2024-09-24 02:36:59 -0300

    feat: improve types and emit events

--- packages/yjs/src/provider.ts
+++ packages/yjs/src/provider.ts
@@ -1,28 +1,31 @@
 import * as Y from 'yjs';
-import { Params, ProviderState } from './types';
+import {
+  DocUpdate,
+  Emitter,
+  Message,
+  MessageToHost,
+  MessageToTarget,
+  MyEvents,
+  Params,
+  ProviderEvents,
+  ProviderState,
+  RealtimeRoom,
+} from './types';
 import { Awareness } from './services';
 import { ObservableV2 } from 'lib0/observable';
-import { ProviderStatusEvents } from './common/types/events.types';
-import { Realtime, type Room, type SocketEvent } from '@superviz/socket-client';
+import {
+  ClientState,
+  ConnectionState,
+  PresenceEvent,
+  PresenceEvents,
+  Realtime,
+  type SocketEvent,
+} from '@superviz/socket-client';
 import { config } from './services/config';
 import { createRoom } from './common/utils/createRoom';
 import { HostService } from './services/host';
 
-type MessageToHost = {
-  update: Uint8Array;
-  originId: string;
-};
-
-type MessageToTarget = {
-  update: Uint8Array;
-  targetId: string;
-};
-
-type DocUpdate = {
-  update: Uint8Array;
-};
-
-export class SuperVizYjsProvider extends ObservableV2<any> {
+export class SuperVizYjsProvider extends ObservableV2<MyEvents> {
   public awareness: Awareness;
   public document: Y.Doc;
 
@@ -30,7 +33,7 @@ export class SuperVizYjsProvider extends ObservableV2<any> {
   private state: ProviderState | `${ProviderState}` = ProviderState.DISCONNECTED;
 
   private realtime: Realtime | null = null;
-  private room: Room | null = null;
+  private room: RealtimeRoom | null = null;
 
   private hostService: HostService | null = null;
 
@@ -71,10 +74,14 @@ export class SuperVizYjsProvider extends ObservableV2<any> {
    */
   public destroy = (): void => {
     if (this.state === ProviderState.DISCONNECTED) return;
+    this.emit('destroy', []);
+
     this.awareness?.destroy();
     this.realtime?.destroy();
     this.hostService?.destroy();
     this.doc.off('updateV2', this.onDocUpdate);
+
+    this.removeRoomListeners();
 
     if (this.room) {
       this.room.disconnect();
@@ -99,58 +106,94 @@ export class SuperVizYjsProvider extends ObservableV2<any> {
     this.room = room;
   }
 
-  private listenToRealtimeEvents(): void {
-    this.room.on('update', this.onRemoteDocUpdate);
-    this.room.presence.on('presence.joined-room', this.onLocalJoinRoom);
+  private addRoomListeners(): void {
+    this.room.on(ProviderEvents.UPDATE, this.onRemoteDocUpdate);
+    this.room.presence.on(PresenceEvents.JOINED_ROOM, this.onLocalJoinRoom);
+    this.realtime.connection.on(this.onConnectionChange);
+  }
+
+  private removeRoomListeners(): void {
+    if (this.room) {
+      this.room.off(ProviderEvents.UPDATE, this.onRemoteDocUpdate);
+      this.room.off(ProviderEvents.MESSAGE_TO_HOST, this.onMessageToHost);
+      this.room.off(ProviderEvents.MESSAGE_TO_SPECIFIC_USER, this.onMessageToUser);
+      this.room.off(ProviderEvents.BROADCAST, this.onBroadcast);
+      this.room.presence.off(PresenceEvents.JOINED_ROOM);
+    }
+
+    this.realtime?.connection.off();
   }
 
   private startRealtime(): void {
     this.createRoom();
     this.hostService = new HostService(this.opts.participant.id, this.onHostChange);
-    this.listenToRealtimeEvents();
+    this.addRoomListeners();
   }
 
-  private onRequestHostState = (event: SocketEvent<MessageToHost>): void => {
+  private onMessageToHost = (event: SocketEvent<MessageToHost>): void => {
     if (!this.hostService.isHost) return;
 
     this._synced = false;
 
     const comingUpdate = new Uint8Array(event.data.update);
+    this.onReceiveRealtimeMessage('message-to-host', {
+      update: comingUpdate,
+      originId: event.data.originId,
+    });
+
     this.updateDocument(comingUpdate);
     const update = Y.encodeStateAsUpdateV2(this.doc, comingUpdate);
 
     if (update.length > 0) {
-      // if the content was new to host, it is new to all users
-      this.broadcastUpdate(update);
+      this.beforeSendRealtimeMessage('broadcast', { update });
+      this.room!.emit('broadcast', { update });
       return;
     }
 
-    this.room!.emit<MessageToTarget>('message-to-specific-user', {
+    this.beforeSendRealtimeMessage('message-to-specific-user', {
+      update,
+      targetId: event.data.originId,
+    });
+    this.room!.emit('message-to-specific-user', {
       update,
       targetId: event.data.originId,
     });
   };
 
-  private onMessageFromHost = (msg: SocketEvent<MessageToTarget>): void => {
+  private onMessageToUser = (msg: SocketEvent<MessageToTarget>): void => {
     if (msg.data.targetId !== this.opts.participant.id) return;
 
-    this.updateDocument(new Uint8Array(msg.data.update));
+    const update = new Uint8Array(msg.data.update);
+    this.onReceiveRealtimeMessage('message-to-specific-user', {
+      update,
+      targetId: msg.data.targetId,
+    });
+
+    this.updateDocument(update);
     this._synced = true;
+
+    this.emit('synced', []);
+    this.emit('sync', []);
   };
 
   private fetch = (): void => {
     this._synced = false;
 
     const update = Y.encodeStateAsUpdateV2(this.doc);
+    this.beforeSendRealtimeMessage('message-to-host', {
+      update,
+      originId: this.opts.participant.id,
+    });
 
-    this.room!.emit<MessageToHost>('send-local-sv-to-host', {
+    this.room!.emit('message-to-host', {
       update,
       originId: this.opts.participant.id,
     });
   };
 
   private onDocUpdate = (update: Uint8Array): void => {
-    this.room!.emit<DocUpdate>('update', { update });
+    this.beforeSendRealtimeMessage('update', { update });
+    this.room!.emit('update', { update });
   };
 
   private updateDocument = (update: Uint8Array): void => {
@@ -165,27 +208,46 @@ export class SuperVizYjsProvider extends ObservableV2<any> {
   }
 
   // #region events callbacks
-  private onLocalJoinRoom = (): void => {
-    if (this.state === ProviderState.CONNECTED) return;
+  private onLocalJoinRoom = (event: PresenceEvent): void => {
+    if (this.state === ProviderState.CONNECTED || event.id !== this.opts.participant.id) return;
 
     this.awareness.connect(this.room!);
     this.changeState(ProviderState.CONNECTED);
-    this.emit('status', [ProviderStatusEvents.CONNECTED]);
+    this.emit('connect', []);
 
-    this.room!.on('send-local-sv-to-host', this.onRequestHostState);
-    this.room!.on('message-to-specific-user', this.onMessageFromHost);
-    this.room!.on('broadcast', this.onBroadcast);
+    this.room!.on(ProviderEvents.MESSAGE_TO_HOST, this.onMessageToHost);
+    this.room!.on(ProviderEvents.MESSAGE_TO_SPECIFIC_USER, this.onMessageToUser);
+    this.room!.on(ProviderEvents.BROADCAST, this.onBroadcast);
 
     this.fetch();
   };
 
-  private onRemoteDocUpdate = (update: SocketEvent<DocUpdate>): void => {
-    this.updateDocument(new Uint8Array(update.data.update));
+  private onConnectionChange = (msg: ConnectionState): void => {
+    if (msg.state === ClientState.DISCONNECTED) {
+      this.emit('disconnect', []);
+    }
+  };
+
+  private onRemoteDocUpdate = (event: SocketEvent<DocUpdate>): void => {
+    if (event.presence?.id === this.opts.participant.id) return;
+
+    const update = new Uint8Array(event.data.update);
+    this.onReceiveRealtimeMessage('update', { update });
+
+    this.updateDocument(update);
   };
 
   private onBroadcast = (event: SocketEvent<DocUpdate>): void => {
-    this.onRemoteDocUpdate(event);
+    if (this.hostService?.isHost) return;
+
+    const update = new Uint8Array(event.data.update);
+    this.onReceiveRealtimeMessage('broadcast', { update });
+
+    this.updateDocument(update);
     this._synced = true;
+
+    this.emit('synced', []);
+    this.emit('sync', []);
   };
 
   private onHostChange = (hostId: string): void => {
@@ -202,7 +264,11 @@ export class SuperVizYjsProvider extends ObservableV2<any> {
     this.state = state;
   }
 
-  private broadcastUpdate(update: Uint8Array): void {
-    this.room!.emit<DocUpdate>('broadcast', { update });
-  }
+  private onReceiveRealtimeMessage: Emitter = (name, data): void => {
+    this.emit('message', [{ name, data } as Message]);
+  };
+
+  private beforeSendRealtimeMessage: Emitter = (name, data): void => {
+    this.emit('outgoingMessage', [{ name, data } as Message]);
+  };
 }
--- packages/yjs/src/types.ts
+++ packages/yjs/src/types.ts
@@ -1,3 +1,5 @@
+import type { Room } from '@superviz/socket-client';
+
 export enum EnvironmentTypes {
   DEV = 'dev',
   PROD = 'prod',
@@ -23,3 +25,59 @@ export enum ProviderState {
   CONNECTING = 'connecting',
   DISCONNECTED = 'disconnected',
 }
+
+export enum ProviderEvents {
+  MESSAGE_TO_HOST = 'message-to-host',
+  MESSAGE_TO_SPECIFIC_USER = 'message-to-specific-user',
+  BROADCAST = 'broadcast',
+  UPDATE = 'update',
+}
+
+export type MessageToHost = {
+  update: Uint8Array;
+  originId: string;
+};
+
+export type MessageToTarget = {
+  update: Uint8Array;
+  targetId: string;
+};
+
+export type DocUpdate = {
+  update: Uint8Array;
+};
+
+// different values that the user can receive when event 'message'
+// is emitted based on the event name
+export type Message =
+  | {
+      name: ProviderEvents.MESSAGE_TO_HOST | `${ProviderEvents.MESSAGE_TO_HOST}`;
+      data: { update: Uint8Array; originId: string };
+    }
+  | {
+      name: ProviderEvents.MESSAGE_TO_SPECIFIC_USER | `${ProviderEvents.MESSAGE_TO_SPECIFIC_USER}`;
+      data: { update: Uint8Array; targetId: string };
+    }
+  | { name: ProviderEvents.BROADCAST | `${ProviderEvents.BROADCAST}`; data: { update: Uint8Array } }
+  | { name: ProviderEvents.UPDATE | `${ProviderEvents.UPDATE}`; data: { update: Uint8Array } };
+
+export type MessageCallback = (message: Message) => void;
+
+export type Emitter = <T extends Message['name']>(
+  name: T,
+  data: Extract<Message, { name: T }>['data'],
+) => void;
+
+export type MyEvents = {
+  connect: () => void;
+  disconnect: () => void;
+  synced: () => void;
+  sync: () => void;
+  destroy: () => void;
+
+  message: MessageCallback;
+  outgoingMessage: MessageCallback;
+  state: (state: ProviderState | `${ProviderState}`) => void;
+};
+
+export type RealtimeRoom = Omit<Room, 'emit'> & { emit: Emitter };
