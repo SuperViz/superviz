6d94b1dcb7e623f6b472ce3dfcd93f925d6924fc
Ian Silva <raspincel@gmail.com>
2024-09-28 18:22:50 -0300

    feat(playground): finish monaco + yjs demo

--- apps/playground/src/pages/yjs-monaco-wio.tsx
+++ apps/playground/src/pages/yjs-monaco-wio.tsx
@@ -4,17 +4,56 @@ import { SuperVizYjsProvider } from "@superviz/yjs";
 
 import { MonacoBinding } from "y-monaco";
 import "../styles/yjs.css";
-import Room, { LauncherFacade, WhoIsOnline } from "@superviz/sdk";
+import Room, {
+  type LauncherFacade,
+  type Participant,
+  WhoIsOnline,
+} from "@superviz/sdk";
 import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import Editor from "@monaco-editor/react";
 
 const id = Math.floor(Math.random() * 1000000);
+
+function setStyles(
+  states: Map<number, Record<string, any>>,
+  ids: Set<number>
+): number[] {
+  const stylesheet = document.getElementById("sv-yjs-monaco");
+  let styles = "";
+
+  const idsList = [];
+  for (const [id, state] of states) {
+    if (ids.has(id) || !state.participant) continue;
+    idsList.push(id);
+
+    styles += `
+      .yRemoteSelection-${id},
+      .yRemoteSelectionHead-${id}  {
+        --presence-color: ${state.participant.slot.color};
+      }
+
+      .yRemoteSelectionHead-${id}::after {
+        content: "${state.participant.name}";
+      }
+    `;
+  }
+
+  stylesheet!.innerText = styles;
+
+  return idsList;
+}
+
 export function YjsWithMonaco() {
   const ydoc = useMemo(() => new Y.Doc(), []);
   const [editor, setEditor] = useState<any>(null);
 
+  const [localParticipant, setLocalParticipant] =
+    useState<Partial<Participant>>();
+
   const provider = useRef<SuperVizYjsProvider>(new SuperVizYjsProvider(ydoc));
   const wio = useRef<WhoIsOnline>(new WhoIsOnline());
+
+  const [ids, setIds] = useState(new Set<number>());
 
   const loaded = useRef(false);
   const [room, setRoom] = useState<LauncherFacade | null>(null);
@@ -28,21 +67,43 @@ export function YjsWithMonaco() {
     (async () => {
       if (room) return;
 
-      setRoom(
-        await Room("90xbxrp4tra3hrkbw5y1sq7sw5cj9v", {
-          group: {
-            id: "yjs-monaco",
-            name: "Yjs Monaco",
-          },
-          participant: {
-            id: `ian-yjs-${id}`,
-            name: `Ian Yjs ${id}`,
-          },
-          roomId: "yjs-monaco",
-          environment: "dev",
-          debug: true,
-        })
-      );
+      const newRoom = await Room("90xbxrp4tra3hrkbw5y1sq7sw5cj9v", {
+        group: {
+          id: "yjs-monaco",
+          name: "Yjs Monaco",
+        },
+        participant: {
+          id: `ian-yjs-${id}`,
+          name: `Ian Yjs ${id}`,
+        },
+        roomId: "yjs-monaco",
+        environment: "dev",
+        debug: true,
+      });
+
+      newRoom.subscribe("participant.updated", (data) => {
+        if (!data.slot?.index) return;
+
+        provider.current.awareness.setLocalStateField("participant", {
+          id: data.id,
+          slot: data.slot,
+          name: data.name,
+        });
+
+        setLocalParticipant({
+          id: data.id,
+          slot: data.slot,
+          name: data.name,
+        });
+
+        newRoom.unsubscribe("participant.updated");
+      });
+
+      const style = document.createElement("style");
+      style.id = "sv-yjs-monaco";
+      document.head.appendChild(style);
+
+      setRoom(newRoom);
     })();
 
     return () => {
@@ -54,9 +115,26 @@ export function YjsWithMonaco() {
     if (joinedRoom || !room) return;
     setJoinedRoom(true);
 
+    if (localParticipant) {
+      provider.current.awareness.setLocalStateField(
+        "participant",
+        localParticipant
+      );
+    }
+
+    const updateStyles = () => {
+      const states = provider.current.awareness.getStates();
+      const idsList = setStyles(states, ids);
+
+      setIds(new Set(idsList));
+    };
+
+    provider.current.on("connect", updateStyles);
+    provider.current.awareness.on("update", updateStyles);
+
     room.addComponent(provider.current);
     room.addComponent(wio.current);
-  }, [room, joinedRoom]);
+  }, [room, joinedRoom, setIds, ids, localParticipant]);
 
   const leaveRoom = useCallback(() => {
     if (!joinedRoom || !room) return;
@@ -68,9 +146,7 @@ export function YjsWithMonaco() {
 
   // this effect manages the lifetime of the editor binding
   useEffect(() => {
-    if (!provider || editor == null) {
-      return;
-    }
+    if (!provider || editor == null) return;
 
     const binding = new MonacoBinding(
       ydoc.getText("monaco"),
@@ -101,12 +177,18 @@ export function YjsWithMonaco() {
           Leave room
         </button>
       </div>
-      <div className="bg-[#1e1e1e] shadow-none pt-8 h-[90%] overflow-auto rounded-sm">
+      <div className="bg-[#1e1e1e] shadow-none h-[90%] overflow-auto rounded-sm">
+        <div className="yRemoteSelectionHead"></div>
         <Editor
           defaultValue="// some comment"
           defaultLanguage="typescript"
           onMount={(editor) => {
             setEditor(editor);
+          }}
+          options={{
+            padding: {
+              top: 32,
+            },
           }}
           theme="vs-dark"
         />
