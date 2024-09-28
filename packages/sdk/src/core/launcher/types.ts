066e1ef678a4280c2bf94198c9c8a6b3396bb6ff
Ian Silva <raspincel@gmail.com>
2024-09-28 18:23:52 -0300

    feat: improve typing when subscring to launcher events

--- packages/sdk/src/core/launcher/types.ts
+++ packages/sdk/src/core/launcher/types.ts
@@ -1,15 +1,37 @@
+import { ParticipantEvent } from '../../common/types/events.types';
+import { type Participant } from '../../common/types/participant.types';
 import { SuperVizSdkOptions } from '../../common/types/sdk-options.types';
 import { Observable } from '../../common/utils';
-import { BaseComponent } from '../../components/base';
 
 export interface DefaultLauncher {}
 
+type Events = ParticipantEvent | `${ParticipantEvent}`;
 export interface LauncherOptions
   extends Omit<SuperVizSdkOptions, 'environment' | 'debug' | 'roomId'> {}
 
+type LauncherSubscribe = {
+  (event: `${ParticipantEvent.SAME_ACCOUNT_ERROR}`, callback: () => void);
+  (event: `${ParticipantEvent.LOCAL_JOINED}`, callback: (participant: Participant) => void);
+  (event: `${ParticipantEvent.JOINED}`, callback: (participant: Participant) => void);
+  (event: `${ParticipantEvent.LOCAL_LEFT}`, callback: (participant: Participant) => void);
+  (event: `${ParticipantEvent.LEFT}`, callback: (participant: Participant) => void);
+  (event: `${ParticipantEvent.LIST_UPDATED}`, callback: (participant: Participant[]) => void);
+  (event: `${ParticipantEvent.LOCAL_UPDATED}`, callback: (participant: Participant) => void);
+};
+
+export type LauncherUnsubscribe = {
+  (event: `${ParticipantEvent.SAME_ACCOUNT_ERROR}`, callback?: () => void);
+  (event: `${ParticipantEvent.LOCAL_JOINED}`, callback?: (participant: Participant) => void);
+  (event: `${ParticipantEvent.JOINED}`, callback?: (participant: Participant) => void);
+  (event: `${ParticipantEvent.LOCAL_LEFT}`, callback?: (participant: Participant) => void);
+  (event: `${ParticipantEvent.LEFT}`, callback?: (participant: Participant) => void);
+  (event: `${ParticipantEvent.LIST_UPDATED}`, callback?: (participant: Participant[]) => void);
+  (event: `${ParticipantEvent.LOCAL_UPDATED}`, callback?: (participant: Participant) => void);
+};
+
 export interface LauncherFacade {
-  subscribe: typeof Observable.prototype.subscribe;
-  unsubscribe: typeof Observable.prototype.unsubscribe;
+  subscribe: LauncherSubscribe;
+  unsubscribe: LauncherUnsubscribe;
   destroy: () => void;
   addComponent: (component: any) => void;
   removeComponent: (component: any) => void;
