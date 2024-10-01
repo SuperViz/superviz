cdb0d4a617dc89733662bb74b2142264a230dbf8
Ian Silva <raspincel@gmail.com>
2024-10-01 07:00:32 -0300

    ci: remove unnecessary loaders

--- packages/yjs/.esbuild/config.js
+++ packages/yjs/.esbuild/config.js
@@ -1,12 +1,4 @@
 const config = {
-  loader: {
-    '.png': 'file',
-    '.svg': 'file',
-    '.woff': 'file',
-    '.woff2': 'file',
-    '.eot': 'file',
-    '.ttf': 'file',
-  },
   bundle: true,
   color: true,
   minify: false,
