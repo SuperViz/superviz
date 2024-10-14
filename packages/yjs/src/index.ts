e7ca2df25dafadb87b69ca865386b3a7c2bde368
Ian Silva <raspincel@gmail.com>
2024-10-14 08:33:45 -0300

    feat: import provider for cdn

--- packages/yjs/src/index.ts
+++ packages/yjs/src/index.ts
@@ -1,2 +1,8 @@
-export { SuperVizYjsProvider } from "./provider";
+import { SuperVizYjsProvider } from "./provider";
 export { Awareness } from "./services";
+
+export { SuperVizYjsProvider };
+
+if (typeof window !== undefined) {
+  window.SuperVizYjsProvider = SuperVizYjsProvider;
+}
