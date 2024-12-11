49b3a6e66a96860b74ec33167a0fda1b6b09f188
Carlos <carlos.santos74@outlook.com>
2024-12-11 13:57:48 -0300

    test: fix the mock test in the room package

--- packages/room/src/index.test.ts
+++ packages/room/src/index.test.ts
@@ -2,7 +2,7 @@ import { execSync } from 'child_process';
 
 describe('SuperViz Room package', () => {
   it('should log the correct message', () => {
-    const output = execSync('node /Users/carlossantos/BACK/SuperViz/superviz/packages/room/src/index.ts').toString();
+    const output = execSync('node ./src/index.ts').toString();
     expect(output).toBe('[SuperViz] Hello from SuperViz Room package\n');
   });
 });
