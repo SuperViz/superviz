5c2e34ff41c17b1309241cdb5b944d6efa330aeb
Carlos <carlos.santos74@outlook.com>
2025-02-19 15:01:25 -0300

    feat: adding the video cdn

--- packages/video/src/shims.d.ts
+++ packages/video/src/shims.d.ts
@@ -1,5 +1,9 @@
 declare global {
   interface Window {
     SuperViz: Record<string, unknown>;
+    SuperVizVideo: {
+      VideoConference: typeof import('./components/video-conference').VideoConference;
+      VideoHuddle: typeof import('./components/video-huddle').VideoHuddle;
+    };
   }
 }
