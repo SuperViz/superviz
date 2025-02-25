239a7938899ac3a05e4884d7a26b939493222320
Carlos <carlos.santos74@outlook.com>
2025-02-25 09:02:50 -0300

    fix: renaming participant events and exporting meeting state enum

--- packages/video/src/index.ts
+++ packages/video/src/index.ts
@@ -6,6 +6,7 @@ import { VideoConference } from './components/video-conference';
 import { VideoHuddleProps } from './components/video-huddle/types';
 import { VideoHuddle } from './components/video-huddle';
 import { VideoEvent } from './components/base/types';
+import { MeetingState } from './common/types/events.types';
 
 
 if (typeof window !== 'undefined') {
@@ -14,7 +15,8 @@ if (typeof window !== 'undefined') {
     VideoConference: VideoConference,
     VideoHuddle: VideoHuddle,
     VideoEvent: VideoEvent,
+    MeetingState: MeetingState,
   };
 }
 
-export { VideoConferenceProps, VideoConference, VideoHuddleProps, VideoHuddle, VideoEvent };
+export { VideoConferenceProps, VideoConference, VideoHuddleProps, VideoHuddle, VideoEvent, MeetingState };
